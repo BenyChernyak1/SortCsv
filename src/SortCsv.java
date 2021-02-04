9721e1cc6850e6b49d855a1c1fb003dfe45042d7
BenyChernyak1 <19Beny03>
2021-02-04 09:25:42 +0200

    methods

--- src/SortCsv.java
+++ src/SortCsv.java
@@ -1,6 +1,5 @@
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStream;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -29,49 +28,55 @@ public class SortCsv {
                     .limit(1)
                     .collect(Collectors.toList()).get(0);
 
-            int skipRows = 1; // for the header
-            int fileNumber = 1;
-            SortedMap<String, List<String>> collect;
-            do {
-                collect = Files.lines(Paths.get(path))
-                        .skip(skipRows)
-                        .limit(maxLinesNumber)
-                        .collect(Collectors.groupingBy(
-                                l -> String.valueOf(l.split(",")[columnNumber]),
-                                TreeMap::new, Collectors.toList()));
-                if (collect.size() != 0) {
-                    fileOperations.writeToFile(collect, fileNumber++);
-                    skipRows += maxLinesNumber;
-                }
-            } while(collect.size() != 0);
+            writeToFiles(fileOperations, path, columnNumber, maxLinesNumber);
 
-
-
-            List<String> filesToMerge = Files.walk(Paths.get(outputPath))
-                    .filter(p -> Files.isRegularFile(p) && p.getFileName().toString().endsWith(".csv"))
-                    .map(p -> p.getFileName().toString())
-                    .collect(Collectors.toList());
-
-            while(filesToMerge.size() > 1) {
-                String file1 = outputPath + File.separator + filesToMerge.get(0);
-                String file2 = outputPath + File.separator + filesToMerge.get(1);
-                if (filesToMerge.size() == 2) {
-                    fileOperations.doMerge(file1, file2, columnNumber, header);
-                } else {
-                    fileOperations.doMerge(file1, file2, columnNumber, null);
-                }
-                // remove files
-                Files.delete(Paths.get(file1));
-                Files.delete(Paths.get(file2));
-
-                filesToMerge = Files.walk(Paths.get(outputPath))
-                        .filter(p -> Files.isRegularFile(p) && p.getFileName().toString().endsWith(".csv"))
-                        .map(p -> p.getFileName().toString())
-                        .collect(Collectors.toList());
-            }
+            mergeToFiles(fileOperations, outputPath, columnNumber, header);
 
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static void mergeToFiles(FileOperations fileOperations, String outputPath, int columnNumber, String header) throws IOException {
+        List<String> filesToMerge = Files.walk(Paths.get(outputPath))
+                .filter(p -> Files.isRegularFile(p) && p.getFileName().toString().endsWith(".csv"))
+                .map(p -> p.getFileName().toString())
+                .collect(Collectors.toList());
+
+        while(filesToMerge.size() > 1) {
+            String file1 = outputPath + File.separator + filesToMerge.get(0);
+            String file2 = outputPath + File.separator + filesToMerge.get(1);
+            if (filesToMerge.size() == 2) {
+                fileOperations.doMerge(file1, file2, columnNumber, header);
+            } else {
+                fileOperations.doMerge(file1, file2, columnNumber, null);
+            }
+            // remove files
+            Files.delete(Paths.get(file1));
+            Files.delete(Paths.get(file2));
+
+            filesToMerge = Files.walk(Paths.get(outputPath))
+                    .filter(p -> Files.isRegularFile(p) && p.getFileName().toString().endsWith(".csv"))
+                    .map(p -> p.getFileName().toString())
+                    .collect(Collectors.toList());
+        }
+    }
+
+    private static void writeToFiles(FileOperations fileOperations, String path, int columnNumber, int maxLinesNumber) throws Exception {
+        int skipRows = 1; // for the header
+        int fileNumber = 1;
+        SortedMap<String, List<String>> collect;
+        do {
+            collect = Files.lines(Paths.get(path))
+                    .skip(skipRows)
+                    .limit(maxLinesNumber)
+                    .collect(Collectors.groupingBy(
+                            l -> String.valueOf(l.split(",")[columnNumber]),
+                            TreeMap::new, Collectors.toList()));
+            if (collect.size() != 0) {
+                fileOperations.writeToFile(collect, fileNumber++);
+                skipRows += maxLinesNumber;
+            }
+        } while(collect.size() != 0);
     }
 }
